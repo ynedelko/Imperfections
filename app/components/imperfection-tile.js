e495efad31d7e49d4d9a2888a2d9ae37d6e66e68
Yelena Nedelko & carlo de gregorio <student@epicodus.com>
2015-09-30 15:32:38 -0700

    add ability to delete imperfection

--- app/components/imperfection-tile.js
+++ app/components/imperfection-tile.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    delete(imperfection) {
+      if (confirm('Are you sure you want to delete?')) {
+        this.sendAction("destroyImperfection", imperfection);
+      }
+    }
+  }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -3,5 +3,12 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('imperfection');
+  },
+
+  actions: {
+    destroyImperfection(imperfection) {
+      imperfection.destroyRecord();
+      this.transitionTo('index');
+    }
   }
 });
